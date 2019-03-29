9a365e1ab6c901e154f95348636dc0336f02d4dd
Shannon Elmore <shannonelmore@Shannons-MBP.attlocal.net>
2019-03-29 02:55:20 -0400

    updated javascript

--- app.js
+++ app.js
@@ -1,40 +1,19 @@
+/*Destination Generator - Randomly Suggest New Destinations*/
 
-// hide destination 
-// when wander button pressed 
-// show suggestion
-// hide wander button
+var newDestination= []
+newDestination[0] = "Bermuda";
+newDestination[1] = "Banff";
+newDestination[2] = "Iceland";
+newDestination[3] = "Berlin";
+newDestination[4] = "Sydney";
+newDestination[5] = "Barcelona";
+newDestination[6] = "New Orleans";
+newDestination[7] = "Rome";
+newDestination[8] = "London";
+newDestination[9] = "Croatia";  
 
-
-
-var NumberOfWords = 10
-
-var words = new BuildArray(NumberOfWords)
-
-// Use the following variables to 
-// define your random words:
-words[1] = "Bermuda"
-words[2] = "Banff"
-words[3] = "Iceland"
-words[4] = "Havana"
-words[5] = "Sydney"
-words[6] = "Barcelona"
-words[7] = "New Orleans"
-words[8] = "Rome"
-words[9] = "London"
-words[10] = "Croatia"
-
-
-function BuildArray(size){
-this.length = size
-for (var i = 1; i <= size; i++){
-this[i] = null}
-return this
+function newDestination(){
+    var randomDestination = Math.floor(Math.random()*(newDestination.length));
+    document.getElementById("pdestination").innerHTML=newdestination[randomDestination];
 }
 
-function PickRandomWord(frm) {
-// Generate a random number between 1 and NumberOfWords
-var rnd = Math.ceil(Math.random() * NumberOfWords)
-
-// Display the word inside the text box
-frm.WordBox.value = words[rnd]
-}
